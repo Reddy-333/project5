e3672061e9f3d0dde88dae63f3cc1b9a42e44db2
Reddy yogeswari <92663176+Reddy-333@users.noreply.github.com>
2023-07-13 23:51:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var image=document.getElementById("image");
+function bulb_on(){
+  image.src="https://th.bing.com/th/id/OIP.565TcOat3QSHtMsuwdW4WwHaHk?pid=ImgDet&rs=1" ;
+}
+function bulb_off(){
+  image.src="https://th.bing.com/th/id/OIP.maWRvjA73l5CT719MqvdSAHaHk?w=205&h=209&c=7&r=0&o=5&pid=1.7" ;
+}
